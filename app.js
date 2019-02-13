60190a44720aaf2a3def09990f3e452dd31d7282
David <9396157+davidguerreiro@users.noreply.github.com>
2019-02-13 15:32:06 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+    // Get references to elements on the page.
+    var form = document.getElementById("message-form");
+    var messageField = document.getElementById("message");
+    var messageList = document.getElementById("messages");
+    var socketStatus = document.getElementById("status");
+    var closeBtn = document.getElementById("close");
+
+    // Create a new WebSocket.
+    var socket = new WebSocket("ws://echo.websocket.org");
+
+    // Show a connected message when the WebSocket is opened.
+    socket.onopen = function(event) {
+        socketStatus.innerHTML = "Connected to " + event.currentTarget.url;
+        socketStatus.className = "open";
+    };
+
+    // Handle any errors that occur.
+    socket.onerror = function(error) {
+        console.log("WebSocket Error: " + error);
+    };
+
+    // Send message when the form is submitted.
+    form.onsubmit = function(e) {
+        e.preventDefault();
+
+        // Retrieve message from textarea.
+        var message = messageField.value;
+
+        // Send message trought the web socket.
+        socket.send(message);
+
+        // Add the message to the message list.
+        messageList.innerHTML += "<li class='sent'><span>Sent:</span> " + message + "</li>";
+
+        // Clear out the message field.
+        messageField.value = '';
+
+        return false;
+    };
+
+    // Handle messages sent by the server.
+    socket.onmessage = function(event) {
+        var message = event.data;
+        messageList.innerHTML += "<li class='received'><span>Received:</span>" + message + "</li>";
+    };
+
+    // Show a disconnected message when the webSocket is closed.
+    socket.onClose = function(event) {
+        socketStatus.innerHTML = "Disconnected from WebSocket.";
+        socketStatus.className = "closed";
+    };
+
+    // Close the WebSocket connection when the close button is clicked.
+    closeBtn.onclick = function(e) {
+        e.preventDefault();
+
+        // Close the webSocket.
+        socket.close();
+
+        return false;
+    };
+};
